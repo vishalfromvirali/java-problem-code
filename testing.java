0cd30264f715ba739e436933b249a6dbe4ad893c
vishalfromvirali <vishalfromvirali@gmail.com>
2024-11-07 01:05:40 +0530

    Add files via upload

---
+++ testing.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class testing{
+    public static void main(String[]args){
+        String str1 = new  String();
+        str1 =str1+"hello vishal cheif";
+        
+        String str2 = new String("vishal");
+        String result1 = str1.toUpperCase();
+
+        System.out.println(result1);
+
+        
+}
+}
